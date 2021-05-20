e855fb6ccdd9465de0d704660bf39e2916bc82c6
BHANU <bhanu.kari@quantiphi.com>
2021-05-20 10:41:53 +0530

    added comments

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-var xttp = new XMLHttpRequest();
-var battles = [];
-var most_active = {};
+var xttp = new XMLHttpRequest();// XMLHttpRequest objects are used to interact with servers.
+var battles = [];//Used for saving the data from json.
+var most_active = {};//Used it for filtering the names of battles.
 var result = {};
 
 var data = new Promise((resolve,reject)=>{
-    xttp.open("GET","battles.json",true);
+    xttp.open("GET","battles.json",true);//Specifies the request
+    //Below function to be executed when the request completes successfully.
     xttp.onload = function() {
     resolve(xttp.responseText);
     };
-    xttp.send();
+    xttp.send();//Sends the request to the server Used for GET requests
 });
+
+//function is to group the array of objects on property.
 function groupBy(objectArray, property) {
     return objectArray.reduce((acc, obj) => {
        const key = obj[property];
        if (!acc[key]) {
           acc[key] = [];
        }
-       // Add object to list for given key's value
        acc[key].push(obj);
        return acc;
     }, {});
  }
-
+//This function is to get the most_active after grouping
  function find(arrayObject,property){
     let keys = Object.keys(arrayObject);
     let max = 0;
@@ -36,7 +38,7 @@ function groupBy(objectArray, property) {
  }
 
 data.then((data)=>{
-    battles = JSON.parse(data);
+    battles = JSON.parse(data);//assigning data to battles array.
     console.log(battles); 
 }).then(()=>{
     let activeKing = groupBy(battles, 'attacker_king');
@@ -50,6 +52,7 @@ data.then((data)=>{
                     'defender_king':find(defenderKing,'defender_king'),
                     'region':find(regionName,'region')
                 };
+    //Below filter is used to filter the battles array with most_active object.
     let nameList = battles.filter(item => {
         for (let key in most_active) {
             if (item[key] === undefined || item[key] != most_active[key])
@@ -57,20 +60,24 @@ data.then((data)=>{
         }
         return true;
     });
-    nameList = nameList.map(val => val.name);
+    nameList = nameList.map(val => val.name);//getting the array of battle_names.
     most_active = {
                 ...most_active,
                 'name':nameList
             }
+    //making an array of defender_size for calculating the min,max and avg.
     let defenderSize = battles.map(val => val.defender_size);
+    // sum is total sum of defender_size.
     let sum = battles.reduce((acc,val)=>{
         return acc = acc + val.defender_size;
     },0);
+    //below three lines is making a array of battle type and triming that array for emplty values.
     let battleNames = Object.keys(battleType);
     battleNames = battleNames.filter((val)=>{
         return val != "";
     })
     console.log(battleNames);
+
     result = {...result,
                 'most_active':most_active,
                 'attacker_outcome':{
